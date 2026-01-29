400546180d4260c972ebc31f3e3190c96e98f663
hssafdar <hamzasyedsafdar@gmail.com>
2026-01-28 20:20:06 -0500

    Initial Commit

---
+++ aiomedia/aiomediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aiomediaApp.swift
+//  aiomedia
+//
+//  Created by Hamza S on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct aiomediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
